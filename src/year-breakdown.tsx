b35fbe6e50f71ff6df1539d0c0bffb7ef95c9298
Ivan Zamanov <ivan.zamanov@ocado.com>
2021-08-03 23:02:33 +0300

    Style year slider according to design (almost)

--- src/year-breakdown.tsx
+++ src/year-breakdown.tsx
@@ -2,25 +2,28 @@ import Slider, { createSliderWithTooltip } from "rc-slider";
 import { useContext } from "react";
 import SelectedYearContext from "./selected-year-context";
 import "rc-slider/assets/index.css";
+import "./slider.css";
 
-const trackStyle: React.CSSProperties = {};
 const handleStyle: React.CSSProperties = {
-  background: "#0678A9 0% 0% no-repeat padding-box",
+  backgroundColor: "#0678A9",
   boxShadow: "0px 3px 6px #00000029",
   borderColor: "#0678A9",
   borderRadius: 10,
-  width: 20,
-  height: 20,
-  bottom: -2,
+  width: 18,
+  height: 18,
+  bottom: -4,
   marginLeft: 5,
 };
 const dotStyle: React.CSSProperties = {
-  width: 20,
-  height: 20,
-  bottom: -8,
+  visibility: "hidden",
+};
+const trackStyle: React.CSSProperties = {
+  backgroundColor: "#BFBFBF",
+  height: 8
 };
 const railStyle: React.CSSProperties = {
-  background: "#E4E4E4 0% 0% no-repeat padding-box",
+  background: "#E4E4E4",
+  height: 8,
   borderRadius: 4,
 };
 
@@ -29,9 +32,7 @@ const TooltipSlider = createSliderWithTooltip(Slider);
 const YearBreakdown: React.FunctionComponent = () => {
   const selectedYear = useContext(SelectedYearContext);
   let marks: any = {};
-  selectedYear.availableYears.forEach(
-    (i) => (marks[i] = { label: String(i) })
-  );
+  selectedYear.availableYears.forEach((i) => (marks[i] = { label: String(i) }));
   return (
     <>
       <TooltipSlider
@@ -48,7 +49,6 @@ const YearBreakdown: React.FunctionComponent = () => {
           1990
         )}
         value={selectedYear.selectedYear}
-        dots={true}
         dotStyle={dotStyle}
         marks={marks}
         trackStyle={trackStyle}
